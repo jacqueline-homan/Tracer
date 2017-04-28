149d0f29e46ab15866f0c201ad6875f4a3919663
Evan Cofsky <evan@theunixman.com>
2017-04-28 01:10:32 -0500

    Add request logging of salient items.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,27 @@
 module Main where
 
 import Lawless
+import Data.Time.Format
+import Textual
 import IO
 
 import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
 
+printTime =
+    print ∘ formatTime defaultLocale (iso8601DateFormat (Just "%H:%M:%S.%q%z"))
+
+logRequest ∷ LogAccess
+logRequest host user time requestLine responseCode size referer userAgent =
+    putStrLn $ hsep [
+    printTime time,
+    referer,
+    brackets requestLine,
+    brackets referer,
+    brackets userAgent
+    ]
+
+serverConfig ∷ Conf
+serverConfig = nullConf {logAccess = Just logRequest}
+
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello, World!"
+main = simpleHTTP serverConfig $ ok "Hello, World!"
